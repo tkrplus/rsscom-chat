e1ff89f96ceddd49372b2adb567a05fb2b318454
tkrplus <niwa.buffer@gmail.com>
2019-01-21 09:59:42 +0900

    firebase settings

--- src/utils/FirebaseUtil.js
+++ src/utils/FirebaseUtil.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: 'AIzaSyA_-0upVEcrmnZLTXuzL20cf-X6rqASccQ',
-  authDomain: 'realtime-todo-demo.firebaseapp.com',
-  databaseURL: 'https://realtime-todo-demo.firebaseio.com',
-  projectId: 'realtime-todo-demo',
-  storageBucket: 'realtime-todo-demo.appspot.com',
-  messagingSenderId: '403516596471'
+  apiKey: 'AIzaSyDtogwhTCG2CWoe-3Zrtj0J7IX5xYQrgRw',
+  authDomain: 'rss-libertas.firebaseapp.com',
+  databaseURL: 'https://rss-libertas.firebaseio.com',
+  projectId: 'rss-libertas',
+  storageBucket: 'rss-libertas.appspot.com',
+  messagingSenderId: '860907068980'
 }
 
 firebase.initializeApp(config)
